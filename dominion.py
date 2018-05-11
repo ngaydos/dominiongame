873fd8dba759d280aa75714736fc0baf7a789690
Nicholas Gaydos <ngaydos@gmail.com>
2018-05-11 14:56:16 -0700

    created cards class and modified

--- dominion.py
+++ dominion.py
@@ -31,7 +31,7 @@ class Player:
                 self.deck.cards.remove(card)
 
     def shuffle(self):
-        self.deck.cards.append += self.discard.cards
+        self.deck.cards += self.discard.cards
         self.discard.cards = []
 
 
@@ -42,8 +42,12 @@ class Player:
 class Deck:
 
     def __init__(self):
-        self.cards = ['copper', 'copper', 'copper', 'copper', 'copper', 'copper', 'copper', 'estate', 'estate', 'estate']
+        self.cards = [Card('copper', ['money'], 0, 0, 0, 1), Card('copper', ['money'], 0, 0, 0, 1), Card('copper', ['money'], 0, 0, 0, 1), Card('copper', ['money'], 0, 0, 0, 1),
+         Card('copper', ['money'], 0, 0, 0, 1), Card('copper', ['money'], 0, 0, 0, 1), Card('copper', ['money'], 0, 0, 0, 1), 
+         Card('estate', ['victorypoint'], 2, 0, 0, 0, 1), Card('estate', ['victorypoint'], 2, 0, 0, 0, 1), Card('estate', ['victorypoint'], 2, 0, 0, 0, 1)]
 
+    def __call__(self):
+        return [card.name for card in self.cards]
 
 
 class Hand:
@@ -51,7 +55,36 @@ class Hand:
     def __init__(self):
         self.cards = []
 
+    def __call__(self):
+        return [card.name for card in self.cards]
+
 class Discard:
 
     def __init__(self):
         self.cards = []
+
+    def __call__(self):
+        return [card.name for card in self.cards]
+
+class Card:
+
+    def __init__(self, name, ctypes, cost, actions, draw, gold, vps =0):
+        self.name = name
+        self.ctypes = ctypes
+        self.cost = cost
+        self.actions = actions
+        self.draw = draw
+        self.gold = gold
+        self.vps = vps
+
+    def __call__(self):
+        return self.name
+
+#Structural Notes:
+
+'''You could set up a card as a class containing data on cost, name, vps and possibly type. 
+This makes playing the cards easier but generates some issues. Alternately you could go with the less
+systemic option and generate a specific function response for each card.
+
+I think to be able to use a machine learning system each card is going to need to be an instance of a card class.'''
+
