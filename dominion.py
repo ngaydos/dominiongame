a5dbe2d437306126455be1725dae8ea6f734454f
Nicholas Gaydos <ngaydos@gmail.com>
2018-05-10 22:06:39 -0700

    updated dominion

--- dominion.py
+++ dominion.py
@@ -1,3 +1,5 @@
+import random
+
 class Game:
 
     def __init__(self, player_count):
@@ -9,7 +11,27 @@ class Player:
     def __init__(self):
         self.hand = Hand()
         self.deck = Deck()
+        self.discard = Discard()
         self.vps = None
+
+    def draw(count = 1):
+        if len(self.deck.cards) < count:
+            remaining = count - len(self.deck.cards)
+            self.draw(len(self.deck.cards))
+            if len(self.discard.cards) < count:
+                self.shuffle()
+                self.draw(len(deck.cards))
+            else:
+                self.shuffle()
+                self.draw(remaining)
+        else:
+            card = random.choice(self.deck.cards)
+            self.hand.cards.append(card)
+            self.deck.cards.remove(card)
+
+    def shuffle():
+        self.deck.cards.append(self.discard.cards)
+        self.discard.cards = []
 
 class Deck:
 
@@ -23,3 +45,7 @@ class Hand:
     def __init__(self):
         self.cards = []
 
+class Discard:
+
+    def __init__(self):
+        self.cards = []
