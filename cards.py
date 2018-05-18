4500343206598edb823bdeda51f4aded849f2094
Nicholas Gaydos <ngaydos@gmail.com>
2018-05-18 13:28:30 -0700

    added more cards

--- cards.py
+++ cards.py
@@ -1,6 +1,6 @@
 class Card:
 
-    def __init__(self, name, ctypes, cost, actions, draw, money, vps =0):
+    def __init__(self, name, ctypes, cost, actions, draw, money, buys= 0, vps =0):
         self.name = name
         #card types as a list allows for the multiple card types introduced in intrigue
         self.ctypes = ctypes
@@ -13,15 +13,18 @@ class Card:
     def __call__(self):
         return self.name
 
-curse = Card('curse', ['curse'], 0, 0, 0, 0, -1)
 copper = Card('copper', ['money'], 0, 0, 0, 1)
 silver = Card('silver', ['money'], 3, 0, 0, 2)
 gold = Card('gold', ['money'], 6, 0, 0, 3)
-estate = Card('estate', ['victorypoint'], 2, 0, 0, 0, 1)
-duchy = Card('duchy', ['victorypoint'], 5, 0, 0, 0, 3)
-province = Card('province', ['victorypoint'], 8, 0, 0, 0, 6)
+estate = Card('estate', ['victorypoint'], 2, 0, 0, 0, vps=1)
+duchy = Card('duchy', ['victorypoint'], 5, 0, 0, 0, vps=3)
+province = Card('province', ['victorypoint'], 8, 0, 0, 0, vps=6)
+curse = Card('curse', ['curse'], 0, 0, 0, 0, vps= -1)
 village = Card('village', ['action'], 3, 2, 1, 0)
 smithy = Card('smithy', ['action'], 4, 0, 3, 0)
+festival = Card('festival'['action'], 5, 2, 0, 2, buys = 1)
+laboratory = Card('laboratory',['action'], 5, 1, 2, 0)
+market = Card('market',['action'], 5, 1, 1, 1, buys = 1)
 
 string_to_card = {'copper': copper, 'silver': silver, 'gold': gold, 'estate': estate, 
 'duchy': duchy, 'province': province, 'smithy': smithy, 'village': village, 'curse': curse}
